db651bd069bdcada0ed41715781b80784a4d4174
Connie Park <conniedc1206@gmail.com>
2022-07-01 10:16:18 -0700

    commit tree 0422601d600db11aa703196e2a94d9a4fcb64155

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ActiveRecord::Base
-  
+    # we can easily access a list of all reviews associated with any game
+    # has_many macros method: PLURAL
+    has_many :reviews
+
+    # to write this #reviews instance method out by hand:
+    # def reviews
+    #     Review.where(game_id: self.id)
+    # end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
-  
+    # to access all the data about the game that's being reviewed, even though that information is stored in another table: use belongs_to macro
+    
+    # using belongs_to macro: the name of the symbol we are passing to belongs_to must be SINGULAR
+    belongs_to :game
+
+    # using custom method: 
+    # def game
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+    # end
 end
